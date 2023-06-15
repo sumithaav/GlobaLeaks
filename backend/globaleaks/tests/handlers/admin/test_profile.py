15cb0cdd2f5e3f24b2fa10b3cb3742a593800fef
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-06-15 14:41:31 +0530

    Add * test function for invalid profile json * test function for delete profile

--- backend/globaleaks/tests/handlers/admin/test_profile.py
+++ backend/globaleaks/tests/handlers/admin/test_profile.py
@@ -23,12 +23,47 @@ class TestProfilesCollection(helpers.TestCollectionHandler):
     }
 
     @inlineCallbacks
+    def test_post_invalid_json(self):
+        self.test_data_dir = os.path.join(helpers.DATA_DIR, 'profiles')
+
+        invalid_test_cases = [
+            ('invalid.json', errors.InputValidationError)
+        ]
+
+        for fname, err in invalid_test_cases:
+            new_p = read_json_file(os.path.join(self.test_data_dir, fname))
+            handler = self.request(new_p, role='admin')
+            handler.request.language = None
+            try:
+                yield self.assertFailure(handler.post(), err)
+            except errors.InputValidationError as e:
+                yield self.assertEqual(type(e), err)
+
+
+    @inlineCallbacks
     def test_post_valid_json(self):
         self.test_data_dir = os.path.join(helpers.DATA_DIR, 'profiles')
 
-        new_q = read_json_file(os.path.join(self.test_data_dir, 'valid.json'))
+        new_p = read_json_file(os.path.join(self.test_data_dir, 'valid.json'))
 
-        handler = self.request(new_q, role='admin')
+        handler = self.request(new_p, role='admin')
         handler.request.language = None
 
         yield handler.post()
+
+
+class TestProfileInstance(helpers.TestHandler):
+    _handler = profile.ProfileInstance
+
+    @inlineCallbacks
+    def test_delete(self):
+        """
+        Create a new profile, then attempt to delete it.
+        """
+        self.test_data_dir = os.path.join(helpers.DATA_DIR, 'profiles')
+
+        new_p = read_json_file(os.path.join(self.test_data_dir, 'valid.json'))
+        new_p = yield profile.create_profile(1, None, new_p)
+        
+        handler = self.request(role='admin')
+        yield handler.delete(new_p['id'])
